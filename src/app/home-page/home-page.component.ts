4f1965fe193b589f6de544aea0e6c5428ddb17ad
Daniel Boada <Boada2303@gmail.com>
2022-04-07 12:32:15 -0400

    bubbles

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   constructor() { }
-  ocupations = [ "Front End Developer", "UI/UX Developer", "Programmer", "Software Developer"];
+  ocupations = [ "Front End Developer", "UI/UX Developer", "Front End Engineer", "UI Programmer", "Software Developer", "Computer Engineer"];
   ocupationIndex = 0;
   image = "assets/DanielBoada.png";
 
