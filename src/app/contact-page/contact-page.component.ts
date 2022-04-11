0937e4e40d14d73a4656be8cb6b7de4aa83d59e3
Daniel Boada <Boada2303@gmail.com>
2022-04-11 15:37:34 -0400

    web view

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -9,7 +9,17 @@ export class ContactPageComponent implements OnInit {
 
   constructor() { }
 
+  LinkedInLogo = 'assets/linkedInName.png';
+  github = 'assets/githubName.png';
+
   ngOnInit(): void {
   }
 
+  public redirectTo(website: string) {
+    if (website === 'github') {
+        window.open('https://github.com/daniel-boada/', '_blank');
+    } else if (website === 'linkedIn') {
+        window.open('https://www.linkedin.com/in/danielboada/', '_blank');
+    }
+}
 }
